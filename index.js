44adc8b6dbd0485cdd2409a13a591bef88a0e6e8
codwerk <louisws@outlook.fr>
2021-06-26 15:07:24 +0200

    intégration html/css

---
+++ index.js
@@ -0,0 +1,10 @@
+/*class Question {
+  // new Question('text1',['choix1','choix2','choix3','choix4'], 'choix2')
+  constructor(text, choices, answer){
+    this.text = text,
+    this.choices = choices,
+    this.answer = answer
+  }
+  
+
+}*/
