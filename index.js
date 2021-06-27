9b65e05f15a81a26711af886802255ec056913e8
codwerk <louisws@outlook.fr>
2021-06-27 12:29:16 +0200

    end quiz logic

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ class Question {
 }
 
 let questions = [
-  new Question('Meilleur buteur de l\'equipe de france ? ', ['benzema', 'girous', 'thierry henry', 'zidane'], 'thierry henry'),
+  new Question('Meilleur buteur de l\'equipe de france ? ', ['benzema', 'giroud', 'thierry henry', 'zidane'], 'thierry henry'),
   new Question('Qui marque un double en 98(finale) ? ', ['platini', 'trezeguet', 'thierry henry', 'zidane'], 'zidane'),
   new Question('Qui marque le premier but en 2018(finale) ? ', ['griezmann', 'giroud', 'mbappe', 'zidane'], 'griezmann'),
   new Question('Qui marque le but victorieux en 2000(finale) ? ', ['griezmann', 'trezeguet', 'thierry henry', 'wiltord'], 'trezeguet')
 ];
-
-console.log(questions);
-
 
 class Quizz {
   // new Quizz(questions)
@@ -51,3 +48,65 @@ class Quizz {
     return this.currentQuestionIndex >= this.questions.length; // boolean
   }
 }
+
+// methods pour logique d'affichage
+
+const display = {
+  // cibler de séléments html pour dynamise le contenu
+  targetElt: function(id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  endQuizz: function() {
+    let endQuizz = `
+      <h1>Quizz terminé !</h1>
+      <h3>Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>
+    `;
+
+    this.targetElt("quizz", endQuizz);
+  },
+  question: function(){
+    this.targetElt("question", quiz.getCurrentQuestion().text)
+  },
+  choices: function(){
+    //tableau de choix
+    let choices = quiz.getCurrentQuestion().choices;
+    
+    guessHandler = (id, guess) => {
+      document.getElementById(id).onclick = function(){
+        quiz.guess(guess);
+        quizApp();
+      }
+    }
+
+    for (let index = 0; index < choices.length; index++) {
+      this.targetElt('choice'+index, choices[index]);
+      guessHandler("guess"+index, choices[index]);
+    }
+  },
+  progress: function() {
+    let numberQuestion = quiz.currentQuestionIndex+1;
+    this.targetElt("progress", "Question " + numberQuestion);
+  }
+}
+
+// loqique du jeux
+quizApp = () => {
+  if(quiz.hasEnded()) {
+    //end
+    display.endQuizz();
+  } else {
+    // afficher la question
+    display.question();
+    // afficher les choix
+    display.choices();
+    // la progression
+    display.progress();
+  }
+}
+
+//creez le quizz
+let quiz = new Quizz(questions);
+quizApp();
+
+console.log(quiz);
